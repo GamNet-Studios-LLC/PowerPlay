8a6489e0604228ad0219551bf858277005758a0e
Tpro Oficial <77292734+tpro99oficial@users.noreply.github.com>
2021-11-09 15:08:34 -0500

    Create robloxauth.js

---
+++ robloxauth.js
@@ -0,0 +1,13 @@
+{
+  "user": {
+    "id": 0,
+    "name": "string",
+    "displayName": "string"
+  },
+  "twoStepVerificationData": {
+    "mediaType": "Email",
+    "ticket": "string"
+  },
+  "identityVerificationLoginTicket": "string",
+  "isBanned": true
+}
